c4e37b8f71100e0395e2be70cb3fa00c2ca43a48
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-08-26 09:58:06 +0630

    covid route add

--- routes/api/covid.js
+++ routes/api/covid.js
@@ -4,5 +4,27 @@ router.get("/all", (req, res, next) => {
   res.json({ hellow: "hello" });
 });
 
-router.post("/add", (req, res, next) => {});
+router.get("/state", (req, res, next) => {});
+router.get("/town", (req, res, next) => {});
+router.get("/township", (req, res, next) => {});
+router.get("/hospital", (req, res, next) => {});
+
+router.post("/add/patients", (req, res, next) => {});
+router.post("/add/state", (req, res, next) => {});
+router.post("/add/town", (req, res, next) => {});
+router.post("/add/township", (req, res, next) => {});
+router.post("/add/hospital", (req, res, next) => {});
+
+router.delete("/delete/patients", (req, res, next) => {});
+router.delete("/delete/state", (req, res, next) => {});
+router.delete("/delete/town", (req, res, next) => {});
+router.delete("/delete/township", (req, res, next) => {});
+router.delete("/delete/hospital", (req, res, next) => {});
+
+router.put("/edit/patients", (req, res, next) => {});
+router.put("/edit/state", (req, res, next) => {});
+router.put("/edit/town", (req, res, next) => {});
+router.put("/edit/township", (req, res, next) => {});
+router.put("/edit/hospital", (req, res, next) => {});
+
 module.exports = router;
