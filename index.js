a56dfcbd32d7eee5953af28aa890bce61c2c6a37
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-08-27 07:17:42 +0630

    :3

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const dotenv = require("dotenv");
 const covid = require("./routes/api/covid");
 const User = require("./routes/api/user");
 dotenv.config();
+const PORT = process.env.PORT || 1337;
 const app = express();
 app.use(express.json());
 app.use("/api", covid);
 app.use("", User);
 require("./mongoodb_connect")()
   .then(() => {
-    app.listen(1337, () => {
-      console.log("I am Listening on port 1337");
+    app.listen(PORT, () => {
+      console.log("I am Listening on port ", PORT);
     });
   })
   .catch(console.log);
